2def181cf1d512d242c41b7f082ffb5feac9eaad
takao.tsukahara <t.s.u.k.a.1340849@gmail.com>
2020-01-25 07:47:54 +0000

    user login

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -25,7 +25,7 @@
               {!! Form::label('password_confirmation', 'Confirmation') !!}
               {!! Form::password('password_confirmation', ['class' => 'form-control']) !!}
           </div>
-         {!!Form::submit('sign up', ['class' => btn btn-primary btn-block']) !!}
+         {!! Form::submit('Sign up', ['class' => 'btn btn-primary btn-block']) !!}
     {!! Form::close() !!}     
      </div>
  </div> 
